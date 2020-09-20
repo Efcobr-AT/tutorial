cc570603bbcb74913e2a6828dfcdd0cef143e044
Erick Barreto <erick.barreto@agilethought.com>
2020-09-20 17:31:12 -0500

    Handlebars

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 // este archivo servira para poder arrancar el servico de expres (servidor)
 const express = require('express');
+const exphbs = require('express-handlebars');
 const path = require('path');
 
 // ---Estructura---
@@ -9,6 +10,13 @@ const app = express();
 // Settings  -- lo que quiere que haga expres basada en algunos modulos
 app.set('port', process.env.PORT || 4000);
 app.set('views', path.join(__dirname, 'views') );       //indica que la carpeta viws se encuntra en otro lugar
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');  //motor de plantillas va a ser handlebars
 
 // Middlewares  -- funciones que se van ejecutando a medida que van llegando peticiones, si un usuario hace una peticion al servidor y se requiere algo previo a ello eso esta seccion se ejecuta
 app.use(express.urlencoded({extended: false}));         //formularios qeu llegen convierten en objeto .json para manipular en codigo
@@ -17,7 +25,7 @@ app.use(express.urlencoded({extended: false}));         //formularios qeu llegen
 
 // Routes  -- es el acceso a las rutas
 app.get('/', (req, res) => {
-    res.send('hello world');
+    res.render('index');
 })
 
 // Static files  -- archivos que se encuentran dentro de la carpeta public, que no se requiere authenticacion (seguridad)
